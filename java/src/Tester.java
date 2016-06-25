fb7773f190479f37c358301228e9e77148af0e1b
Shakib Ahmed <shakib.r.ahmed@gmail.com>
2016-06-25 18:59:35 -0400

    Download a single video from a DALN link to local machine

--- java/src/Tester.java
+++ java/src/Tester.java
@@ -6,6 +6,9 @@ public class Tester {
     public static void main(String[] args)
     {
         System.out.println("Hello world");
+        VideoImporter videoImporter = new VideoImporter();
+
+        videoImporter.importVideo("1");
 
     }
 }
--- java/src/VideoImporter.java
+++ java/src/VideoImporter.java
@@ -1,6 +1,38 @@
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 /**
  * Created by Shakib on 6/25/2016.
+ *
+ * The purpose of this class is to retrieve videos stored on OSU's DALN website and upload them to SproutVideo.
+ * Each record in the DALN has a unique identifier, so the method will take this ID as input, find the video(s)
+ * posted in that record (if any), store it temporarily in a server, then upload to SproutVideo using its API.
  */
-public class VideoImporter {
+public class VideoImporter
+{
+    public void importVideo(String ID)
+    {
+        //Download video to local machine
+        URL website = null;
+        Path target = Paths.get("C:\\Users\\Shakib\\Desktop\\video.MOV");
+
+        try {
+            website = new URL("http://daln.osu.edu/bitstream/handle/2374.DALN/1543/Video%203.MP4");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+        try (InputStream in = website.openStream()) {
+            Files.copy(in, target, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
